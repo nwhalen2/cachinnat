cf461174d160eb1a99a7b9e6ce6198bdec592acc
natalie <nattydubs@natalies-MacBook-Pro-2.local>
2025-02-06 13:58:22 -0600

    init vue-project

---
+++ vue-project/src/components/Nav.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
