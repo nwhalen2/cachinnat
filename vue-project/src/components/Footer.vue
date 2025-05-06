ac128ff67d1f8095e8655487d9a6cd48ede61437
natalie <natalie@truefirestudios.com>
2025-05-06 16:18:50 -0500

    pink hr for footer

--- vue-project/src/components/Footer.vue
+++ vue-project/src/components/Footer.vue
@@ -2,7 +2,7 @@
 
 <template>
     <div class="p-4 text-center">
-        <hr />
+        <hr class="border-5 border-pink" />
         <div><small>2025</small></div>
         <img width="24" height="24" src="https://img.icons8.com/emoji/48/spouting-whale.png" alt="whalen"/>
     </div>
