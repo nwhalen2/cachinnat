8e4bfd51999c5fa5f5967ffe7f8e3b5fc82abef7
natalie <natalie@truefirestudios.com>
2025-09-26 10:53:01 -0500

    add horizontal line before recurring shows

--- vue-project/src/views/SiteView.vue
+++ vue-project/src/views/SiteView.vue
@@ -5,6 +5,7 @@ import Monthlies from '../components/Site/Monthlies.vue'
 
 <template>
   <main>
+    <hr class="border-5 border-pink" />
     <Monthlies class="my-4" />
     <hr class="border-5 border-pink" />
     <Shows class="my-4" />
