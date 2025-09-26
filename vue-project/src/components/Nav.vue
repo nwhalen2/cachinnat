79166f302daac03d537fa53a56e9a7a6b48d4d84
natalie <natalie@truefirestudios.com>
2025-09-26 10:09:39 -0500

    update nav intro: standup comedy in chicago & beyond

--- vue-project/src/components/Nav.vue
+++ vue-project/src/components/Nav.vue
@@ -8,21 +8,26 @@ defineProps({
   },
 })
 
-const intro1 = 'hey friends! i\'m a standup comic in chicago.<br/>i run two regular shows every month and perform around chicago & beyond.'
-const intro2 = 'come see me tell jokes <33<br/>'
+const title = `Stand Up Comedy <br/>in Chicago & Beyond`
+//const intro1 = 'hey friends! i\'m a standup comic in chicago.<br/>i run two regular shows every month and perform around chicago & beyond.'
+//const intro2 = 'come see me tell jokes <33<br/>'
 const chirp = '(also i DJ for chirp radio 107.1FM 6-9am every monday morning!!)'
 </script>
 
 <template>
     <div>
-        <div class="d-flex">
+        <div class="text-center border border-warning p-4 col-12 col-md-6 col-lg-4 mx-auto d-flex flex-column align-items-center gap-2">
+            <h5 class="title" v-html="title"></h5>
+            <a href="https://groups.google.com/g/cachinnat-club" target="_blank" class="text-decoration-none bg-orange-25 text-black border rounded border-black border-dotted p-2">join my mailing list</a>
+        </div>
+        <!--div class="d-flex">
             <div class="d-none d-md-block col-md-1 col-lg-3"></div>
             <div class="col-12 col-md-6 col-lg-4 border border border-warning p-4 text-center" v-html="intro1"></div>
         </div>
         <div class="d-flex justify-content-center">
             <div class="col-4 d-md-none"></div>            
             <div class="col-6 col-md-4 border border-3 border-warning p-4 text-center" v-html="intro2"></div>
-        </div>
+        </div-->
         <!--div class="d-flex justify-content-center justify-content-md-end">
             <div class="w-75 w-md-100 float-end border border-warning p-2" v-html="chirp"></div>
         </div-->
