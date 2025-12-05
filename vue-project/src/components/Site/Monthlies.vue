3e352a4e96f7d07cc3563fc6238935156144a8ab
nwhalen2 <78510954+nwhalen2@users.noreply.github.com>
2025-12-04 18:55:11 -0600

    spacing edits for my shows

--- vue-project/src/components/Site/Monthlies.vue
+++ vue-project/src/components/Site/Monthlies.vue
@@ -2,21 +2,21 @@
   <div class="text-center">
     <h5 class="title my-4">hosting these :)</h5>
     <div class="d-flex justify-content-center align-items-center gap-4 m-4">
-      <div class="bg-orange-25 rounded col-6 col-md-4 mx-md-4 p-4 d-flex flex-column">
+      <div class="bg-orange-25 rounded col-6 col-md-4 mx-md-4 p-4 d-flex flex-column gap-2">
         <a 
         href="https://www.instagram.com/crafted.laughs/" 
         target="_blank">
           <img class="img-fluid" src="../../assets/images/craftedlaughs_winter2025.jpeg" alt="crafted laughs in lake geneva" />
         </a>
-        <div class="mt-auto">crafted laughs in lake geneva (winter edition)</div>
+        <div>crafted laughs in lake geneva<br/>(winter edition)</div>
       </div>
-      <div class="bg-pink-25 rounded col-6 col-md-4 mx-md-4 p-4 d-flex flex-column">
+      <div class="bg-pink-25 rounded col-6 col-md-4 mx-md-4 p-4 d-flex flex-column gap-2">
         <a 
         href="https://www.instagram.com/thelovebelowcomedyshow/" 
         target="_blank">
           <img class="img-fluid" src="../../assets/images/lovebelow_generic.jpeg" alt="the love below comedy show" />
         </a>
-        <div class="mt-2">the love below comedy show</div>
+        <div>the love below comedy show</div>
       </div>
     </div>
     <!--div class="d-flex justify-content-center gap-4 m-4">
