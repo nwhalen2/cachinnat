a089439e81700eff6cb0c9bb570436ab22e0bf35
nwhalen2 <78510954+nwhalen2@users.noreply.github.com>
2025-12-31 14:20:22 -0600

    update footer year from 2025 to 2026

--- vue-project/src/components/Footer.vue
+++ vue-project/src/components/Footer.vue
@@ -3,7 +3,7 @@
 <template>
     <div class="p-4 text-center">
         <hr class="border-5 border-pink" />
-        <div><small>2025</small></div>
+        <div><small>2026</small></div>
         <img width="24" height="24" src="https://img.icons8.com/emoji/48/spouting-whale.png" alt="whalen"/>
     </div>
 </template>
